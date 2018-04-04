aa9459d356f2dac1dcf4312cb8a5606f79bf8513
iceandfire1981@live.cn <iceandfire1981@live.cn>
2018-04-04 08:12:35 +0800

    Modify demo application

--- app/src/main/java/com/icen/icenlibs/MainEntryActivity.java
+++ app/src/main/java/com/icen/icenlibs/MainEntryActivity.java
@@ -1,11 +1,15 @@
 package com.icen.icenlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainEntryActivity extends AppCompatActivity {
+
+    public static final String ACTION_BLE_CONFIG = "com.icen.icenlibs.BLE_CONFIG";
+    public static final String ACTION_BLE_DEMO = "com.icen.icenlibs.BLE_DEMO";
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -20,15 +24,21 @@ public class MainEntryActivity extends AppCompatActivity {
     }
 
     public void onClick(View click_view) {
+        Intent start_intent = new Intent();
+        start_intent.setAction(ACTION_BLE_DEMO);
         switch (click_view.getId()){
             case R.id.entry_ble:
+                start_intent.setAction(ACTION_BLE_DEMO);
                 break;
             case R.id.entry_ble_config:
-                
+                start_intent.setAction(ACTION_BLE_CONFIG);
                 break;
             default:
                 break;
         }
+        start_intent.addCategory(Intent.CATEGORY_DEFAULT);
+        startActivity(start_intent);
+
     }
 
     /**
