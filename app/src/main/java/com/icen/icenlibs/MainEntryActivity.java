326a40a98ab5b13cce61d709f695ab1ec10e0718
iceandfire1981@live.cn <iceandfire1981@live.cn>
2018-11-02 09:49:32 +0800

    modify something

--- app/src/main/java/com/icen/icenlibs/MainEntryActivity.java
+++ app/src/main/java/com/icen/icenlibs/MainEntryActivity.java
@@ -40,10 +40,4 @@ public class MainEntryActivity extends AppCompatActivity {
         startActivity(start_intent);
 
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
